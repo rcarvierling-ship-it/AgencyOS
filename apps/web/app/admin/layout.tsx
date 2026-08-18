9dbaff1b0bc9272826a694ab1f1ec88e6d9f1d08
rcarvierling-ship-it <rcar.vierling@gmail.com>
2026-08-18 11:40:42 -0400

    Load IconScout Unicons line icon system for admin

--- apps/web/app/admin/layout.tsx
+++ apps/web/app/admin/layout.tsx
@@ -1,5 +1,8 @@
 import './admin-global.css'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return children
+  return <>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
+    {children}
+  </>
 }
